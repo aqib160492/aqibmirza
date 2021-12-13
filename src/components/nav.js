0304e5d9524be37b823dda1b3b80e436671038e1
Aqib Mirza <aqib160492@gmail.com>
2021-12-13 20:15:51 +0530

    Sitemap

--- src/components/nav.js
+++ src/components/nav.js
@@ -26,11 +26,11 @@ const Navbar = props => {
         <img src={Menu} alt={"Menu"} />
       </button>
       <ul className={props.toggle ? "nav-links show-nav" : "nav-links"}>
-        <li><a href="/#" onClick={(e) => {handleClick(e,"home")}}>Home</a></li>
-        <li><a href="/#about" onClick={(e) => {handleClick(e,"about")}}>About</a></li>
-        <li><a href="/#services" onClick={(e) => {handleClick(e,"services")}}>Services</a></li>
-        <li><a href="/#portfolio" onClick={(e) => {handleClick(e,"portfolio")}}>Portfolio</a></li>
-        <li><a href="/#contact" onClick={(e) => {handleClick(e,"contact")}}>Contact</a></li>
+        <li><a href="/aqibmirza/#" onClick={(e) => {handleClick(e,"home")}}>Home</a></li>
+        <li><a href="/aqibmirza/#about" onClick={(e) => {handleClick(e,"about")}}>About</a></li>
+        <li><a href="/aqibmirza/#services" onClick={(e) => {handleClick(e,"services")}}>Services</a></li>
+        <li><a href="/aqibmirza/#portfolio" onClick={(e) => {handleClick(e,"portfolio")}}>Portfolio</a></li>
+        <li><a href="/aqibmirza/#contact" onClick={(e) => {handleClick(e,"contact")}}>Contact</a></li>
       </ul>
     </div>
   );
